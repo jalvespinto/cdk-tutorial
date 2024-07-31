f153344456e9553fe06e91a274744af8cbee2438
jalvespinto <jalvespinto@gmail.com>
2024-07-31 11:01:00 -0300

    added MyL2Bucket

--- cdk_starter/cdk_starter_stack.py
+++ cdk_starter/cdk_starter_stack.py
@@ -1,19 +1,20 @@
-from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
-)
+from aws_cdk import CfnOutput, Duration, Stack, aws_s3
 from constructs import Construct
+
 
 class CdkStarterStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkStarterQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        myL2Bucket = aws_s3.Bucket(self, "MyL2Bucket",
+            lifecycle_rules=[
+                aws_s3.LifecycleRule(
+                    expiration=Duration.days(365))
+            ]
+        )
+        
+        output = CfnOutput(self, "MyL2BucketOutput",
+            value=myL2Bucket.bucket_name,
+            description="My L2 Bucket"
+        )
